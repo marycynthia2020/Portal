deb2db95be5823d0673f759ac3b5055366d999e2
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-03-28 01:40:40 +0000

    adds keysection and footer section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Blackbutton from '../components/ui/Blackbutton'
 import Whitebutton from '../components/ui/Whitebutton'
+import Keysection from '../components/Keysection'
+import Footer from '../components/layout/Footer'
 
 const Home = () => {
   return (
-    <div>Home
+    <div>HOME
     </div>
 
   )
